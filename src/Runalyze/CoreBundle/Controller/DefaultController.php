f640284f0abd35230b2a3d99f3f91182defb87f7
mipapo <michael@mipapo.de>
2015-04-25 21:27:38 +0200

    adding startpage and minor changes

--- src/Runalyze/CoreBundle/Controller/DefaultController.php
+++ src/Runalyze/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace Runalyze\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Runalyze\CoreBundle\Entity\Account;
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('RunalyzeCoreBundle:Default:index.html.twig');
+    }
+    
+    /**
+     * @Route("/impressum")
+     */
+    public function impressumAction()
+    {
+        return $this->render('RunalyzeCoreBundle:Default:impressum.html.twig');
+    }
+    
+    /**
+    * @Route("/privacy")
+    */
+    public function privacyAction()
+    {
+        return $this->render('RunalyzeCoreBundle:Default:privacy.html.twig');
     }
 }
