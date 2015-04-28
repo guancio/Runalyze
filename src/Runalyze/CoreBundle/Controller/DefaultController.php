f7ccfa660896877f41e9bb9e5f3e757c82856516
mipapo <michael@mipapo.de>
2015-04-28 07:27:07 +0200

    Userstats as a Controller

--- src/Runalyze/CoreBundle/Controller/DefaultController.php
+++ src/Runalyze/CoreBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Runalyze\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Runalyze\CoreBundle\Entity\Account;
+
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -37,5 +38,15 @@ class DefaultController extends Controller
     public function privacyAction()
     {
         return $this->render('RunalyzeCoreBundle:Default:privacy.html.twig');
+    }
+    
+    public function UserStatsAction()
+    {
+       /* $qb = $entityManager->createQueryBuilder();
+        $qb->select('count(account.id)');
+        $qb->from('RunalyzeCoreBundle:Account','account');
+        $count = $qb->getQuery()->getSingleScalarResult();
+*/
+        return $this->render('RunalyzeCoreBundle:Default:userstats.html.twig');
     }
 }
