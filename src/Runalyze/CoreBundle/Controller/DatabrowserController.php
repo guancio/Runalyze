83e47628ea1b1066a01e99081425b6f19c2e9e96
mipapo <michael@mipapo.de>
2015-05-07 20:37:57 +0200

    minor changes

--- src/Runalyze/CoreBundle/Controller/DatabrowserController.php
+++ src/Runalyze/CoreBundle/Controller/DatabrowserController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 use Symfony\Component\HttpFoundation\Request;
-use Runalyze\CoreBundle\Service\DataBrowserTime;
+use Runalyze\CoreBundle\Service\DataBrowserTime as DBTime;
 use Runalyze\CoreBundle\Service\Time;
 
 class DatabrowserController extends Controller
@@ -23,35 +23,48 @@ class DatabrowserController extends Controller
         $title['year'] = date("Y", time());
         $title['week'] = date("W", time());
         $em = $this->getDoctrine()->getManager();
-        $starttime = '1330580400';
-        $endtime = '1431295190';
+        
+        $time = $this->initTimestamps($request);
+        
+        $starttime = $time['start'];
+        $endtime = $time['end'];
+        
+        $backtime = DBTime::prevTimestamps($starttime, $endtime);
+        $nexttime = DBTime::nextTimestamps($starttime, $endtime);
+        dump($nexttime);
+        dump($title);
         
         $trainings = $em->getRepository('RunalyzeCoreBundle:Training')->findByTimeRangeForAccount($starttime, $endtime, $accountid);
         dump($trainings);
+        print_r($trainings);
         return $this->render('RunalyzeCoreBundle:Databrowser:databrowser.html.twig',
-                array('title' => $title));
+                array('title' => $title,
+                      'backtime' => $backtime,
+                      'nexttime' => $nexttime));
     }
     
     /**
      * Init private timestamps from request
      */
-    protected function initTimestamps() {
-            if (!isset($_GET['start']) || !isset($_GET['end'])) {
-                    $Mode = Configuration::DataBrowser()->mode();
-                        
-                    if ($Mode->showMonth()) {
-                            $timestamp_start = mktime(0, 0, 0, date("m"), 1, date("Y"));
-                            $timestamp_end   = mktime(23, 59, 50, date("m")+1, 0, date("Y"));
+    protected function initTimestamps(Request $request) {
+        $dbConf = $this->get('runalyze.configuration')->getCategory('data-browser');
+        dump($dbConf);
+            if (!empty($request->query->get('start')) && !empty($request->query->get('end'))){
+
+                    if ($dbConf['DB_DISPLAY_MODE'] != 'week') {
+                            $rdata['start'] = mktime(0, 0, 0, date("m"), 1, date("Y"));
+                            $rdata['end']   = mktime(23, 59, 50, date("m")+1, 0, date("Y"));
                     } else {
-                            $timestamp_start = Time::Weekstart(time());
-                            $timestamp_end   = Time::Weekend(time());
+                            $rdata['start'] = Time::Weekstart(time());
+                            $rdata['end']   = Time::Weekend(time());
                     }
             } else {
-                    $this->timestamp_start = $_GET['start'];
-                    $this->timestamp_end   = $_GET['end'];
+                   $rdata['start'] = $request->query->get('start');
+                   $rdata['end']   = $request->query->get('end');
             }
-
-            $this->day_count = round(($this->timestamp_end - $this->timestamp_start) / 86400);
+            $rdata['days'] = round(($rdata['end'] - $rdata['start']) / 86400);
+            echo $rdata['days'];
+            return $rdata;
     }
 
 }
--- src/Runalyze/CoreBundle/Service/Configuration.php
+++ src/Runalyze/CoreBundle/Service/Configuration.php
@@ -8,6 +8,47 @@ use Symfony\Component\HttpFoundation\Response;
  * @package Runalyze\Service
  */
 class Configuration {
+
+    private $em;
+
+    private $category;
     
+    private $account;
+    
+    /**
+     * Values from database
+     * @var array
+     */
+    private $ValuesFromDB = null;
+        
+    public function __construct($em)
+    {
+        $this->em = $em->getRepository('RunalyzeCoreBundle:Conf');
+        $this->loadAll();
+    }
+
+    public function loadAll() {
+         $DBconf = $this->em->findAllByAccount('1');
+
+
+         foreach($DBconf as $key => $conf) {
+             $values[$conf->getCategory()][$conf->getKey()] = $conf->getValue();
+        }
+         
+         $this->ValuesFromDB = $values;
+         
+    }
+    
+    public function setCategory($category) {
+        $this->category = $category;
+    }
+    
+    public function getCategory($category) {
+        return $this->ValuesFromDB[$category];
+    }
+    
+    public function get($key) {
+        return $this->ValuesFromDB[$this->category][$key];
+    }
 
 }
